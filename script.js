63a12d824e771f19f4ff93225c3dfe7727a4598b
MhmmYah <yew.m.loy@gmail.com>
2022-10-17 21:23:36 -0400

    finalized style. fixed user input errors. still has errors

--- script.js
+++ script.js
@@ -2,41 +2,80 @@ const display = document.getElementById("answer");
 const funcdict = ["add", "subtract", "multiply", "divide"];
 
 let funcselect = null;
+let inputmem = null;
+let inputuser = '';
+let lastbuttonpressed = null;
+
 const functionbuttons = document.querySelectorAll("div.functions button");
 functionbuttons.forEach((button)=>{
     button.addEventListener('click', ()=>{
-        funcselect = funcdict.indexOf(button.id);
         document.getElementById("dot").disabled = false;
-        if(inputmem){
-            // operate and store back in input mem
-        }else{
-            inputmem = +inputuser;
-            inputuser="";
+        if(lastbuttonpressed == null){
+        } else if (lastbuttonpressed == 0){
+            if(inputmem && funcselect){
+                inputmem = operate(inputmem, inputuser, funcselect);
+                inputuser = "";
+                display.value = inputmem;
+            }else{
+                inputmem = inputuser;
+                inputuser="";
+            }
+            funcselect = funcdict.indexOf(button.id);
+        } else if (lastbuttonpressed == 1){
+            funcselect = funcdict.indexOf(button.id);
+        } else{
+            alert("Wut in Tarnation!")
         }
+        lastbuttonpressed = 1;
     })
 });
 
-let inputmem = null;
-let inputuser = "";
-const valselect = document.querySelectorAll("div.inouts button");
+const valselect = document.querySelectorAll("div.inputs button");
 valselect.forEach((button)=>{
     button.addEventListener('click', ()=>{
         if(button.id == "dot"){
             button.disabled = true;
-            inputuser.concat(".")
+            inputuser += "."
         }else{
-            inputuser.concat(button.id)
+            inputuser += button.id
         }
+        display.value = inputuser
+        lastbuttonpressed = 0;
     })
 });
 
+const evalselect = document.querySelectorAll("div.evaluate button");
+evalselect.forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        switch (button.id) {
+            case "equal":
+                lastbuttonpressed = 1;
+                inputmem = operate(inputmem, inputuser, funcselect);
+                inputuser = "";
+                display.value = inputmem;
+                funcselect = null
+                break;
+            
+            case "clear":
+                lastbuttonpressed = null
+                inputmem = null
+                inputuser = ''
+                display.value = inputuser
+                funcselect = null
+                break;
 
+            default:
+                alert("not Implemented Yet")
+                break;
+        }
+    })
+});
 
 function operate(a, b, funcselect){
     let answer = null;
     switch (funcselect) {
         case 0:
-            answer = a + b;
+            answer = parseFloat(a) + parseFloat(b);
             break;
     
         case 1:
@@ -52,7 +91,8 @@ function operate(a, b, funcselect){
             break;
 
         default:
-            alert("Wut In Tarnation?!?")
+            answer = a;
             break;
     }
+    return answer;
 }
